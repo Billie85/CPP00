cf94b606d673691fa88425b568fcbcd13bd6560b
Angelinne <kayumi85@hotmail.com>
2023-01-03 18:54:08 +0900

    update main

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -5,15 +5,31 @@ int main(void)
 	std::string	s;
 	while (std::cin.eof() == 0)
 	{
+		Contact	input;
+		int count_contact;
 		std::cout << "Enter: -> ---ADD/SEARCH/EXIT--- <- " << std::endl;
 		std::cin >> s;
+
+		count_contact = 0;
 		if (s == "ADD")
-			std::cout << "this is add" << std::endl;
-			else if(s == "SEARCH")
+		{
+			std::cout << "input First Name     : ";
+		 	std::cin >> s;
+			std::cout << "input Last Name      : ";
+			std::cin >> s;
+			std::cout << "input Nick Name      : ";
+			std::cin >> s;
+			std::cout << "input Phone Number   : ";
+			std::cin >> s;
+			std::cout << "input Darkest Secret : ";
+			std::cin >> s;
+			count_contact++;
+		}
+		/* 	else if(s == "SEARCH")
 			std::cout << "this is search" << std::endl;
 			else if(s == "EXIT")
-			std::cout << "this is exit" << std::endl;
+			std::cout << "this is exit" << std::endl; */
 		//break;
 	}
-	
+	return (0);
 }
